82737daeb913930210cbee0868a3c602d7a57b70
Bryan C Walsh <Walsh.C.Bryan@Gmail.com>
2020-01-10 09:57:08 -0600

    mvc-exercise

--- src/main/java/controller/CheckProfileServlet.java
+++ src/main/java/controller/CheckProfileServlet.java
@@ -1,19 +1,21 @@
 package controller;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/CheckProfileServlet")
 public class CheckProfileServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         if (request.getMethod().equalsIgnoreCase("post")) {
             String username = request.getParameter("username");
             String password = request.getParameter("password");
             if (username.equals("admin") && password.equals("password")) {
-                response.sendRedirect("/profile");
+                response.sendRedirect("/profile.jsp");
             } else {
-                response.sendRedirect("/login");
+                response.sendRedirect("/login.jsp");
             }
         }
     }
